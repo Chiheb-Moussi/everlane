34f304e7fbbd8830fda82b04e1e29aa2e2267086
Chiheb Moussi <chihebmoussi@gmail.com>
2023-12-25 20:24:39 +0100

    test

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,8 @@
 import type { Preview } from "@storybook/react";
 import '../src/index.css'; // replace with the name of your tailwind css file
 
+
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
