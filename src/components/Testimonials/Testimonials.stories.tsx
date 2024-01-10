c3b99fbb0433ea4e6e48b0531967a7e188430125
Chiheb Moussi <chihebmoussi@gmail.com>
2024-01-10 21:08:56 +0100

    add testimonials

---
+++ src/components/Testimonials/Testimonials.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Testimonials from "./Testimonials";
+
+const meta = {
+  title: "components/Testimonials",
+  component: Testimonials,
+} as Meta<typeof Testimonials>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  
+   
+};
