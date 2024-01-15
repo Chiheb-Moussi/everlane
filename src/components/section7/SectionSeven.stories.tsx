eb7e5942009f45ecdaee12b538f9cd1f928b1213
Khalil <Khalil.haouas@gmail.com>
2024-01-15 21:01:15 +0100

    Ec-10 finisch

--- src/components/section7/SectionSeven.stories.tsx
+++ src/components/section7/SectionSeven.stories.tsx
@@ -6,4 +6,6 @@ const meta = {
 } satisfies Meta<typeof SectionSeven>;
 export default meta;
 type Story = StoryObj<typeof meta>;
-export const Default: Story = {};
+export const Default: Story = {
+  args:{}
+};
