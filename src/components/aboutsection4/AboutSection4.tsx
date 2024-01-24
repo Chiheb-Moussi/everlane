9d3aa1bd04baef621f01d1372d806f191695e34e
Khalil <Khalil.haouas@gmail.com>
2024-01-24 15:33:26 +0100

    EC-16 aboutSection 4 finish

---
+++ src/components/aboutsection4/AboutSection4.tsx
@@ -0,0 +1,13 @@
+const AboutSection4 = () => {
+  return (
+    <div className="w-full">
+      <img
+        src="/aboutSection4/image.png"
+        className="w-full h-96 object-cover"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default AboutSection4;
