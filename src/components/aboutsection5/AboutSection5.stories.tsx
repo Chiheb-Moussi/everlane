0e276e634be7b205b338476364cbdf734750364d
Khalil <Khalil.haouas@gmail.com>
2024-01-24 15:43:26 +0100

    EC-17 aboutSection5 finish

---
+++ src/components/aboutsection5/AboutSection5.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryObj } from "@storybook/react";
+import AboutSection5 from "./AboutSection5";
+
+const meta = {
+
+  title: "components/AboutSection5",
+  component: AboutSection5,
+
+}satisfies Meta<typeof AboutSection5>;
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+export const Default : Story = {}
