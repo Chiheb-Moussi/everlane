83a303c4be8495805c97cc1dbd619c8f69188b29
Khalil <Khalil.haouas@gmail.com>
2024-01-30 17:53:45 +0100

    landinPageTopMenu Added

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,8 +1,11 @@
 import TopHeader from "../../components/TopHeader/TopHeader.tsx";
+import TopMenu from "../../components/TopMenu/TopMenu.tsx";
 
 const LandingPage = () => {
     return <>
         <TopHeader />
+        <TopMenu/>
+        
     </>
 }
 
