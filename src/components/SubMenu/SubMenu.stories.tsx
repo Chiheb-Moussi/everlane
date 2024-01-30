7643a8c6b7cd91c2c663c28d81bf5a102c019dd6
Chiheb Moussi <chihebmoussi@gmail.com>
2024-01-30 17:47:09 +0100

    add top menu

--- src/components/SubMenu/SubMenu.stories.tsx
+++ src/components/SubMenu/SubMenu.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import MainMenu from './MainMenu';
 
 const meta = {
-    title: 'componets/SubMenu',
+    title: 'components/SubMenu',
     component: MainMenu,
 
 } satisfies Meta<typeof MainMenu>;
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,8 +1,10 @@
 import TopHeader from "../../components/TopHeader/TopHeader.tsx";
+import TopMenu from "../../components/TopMenu/TopMenu.tsx";
 
 const LandingPage = () => {
     return <>
         <TopHeader />
+        <TopMenu />
     </>
 }
 
